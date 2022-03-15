8bb72c052ef8cf104bb85b0a908a8617d7263be8
Rakesh Itape <rakeshitape844@gmail.com>
2022-03-15 16:28:15 +0530

    meld functionality check

--- Undo the changes/src/com/TechM/Demo.java
+++ Undo the changes/src/com/TechM/Demo.java
@@ -5,7 +5,14 @@ public class Demo
 	public static void main(String[] args) 
 	{
 		System.out.println("New Demo class");
-		 
+		System.out.println("This is Java");
+		System.out.println("This is Python");
+		System.out.println("This is C.");
+		System.out.println("This is php");
+		System.out.println("This is C++");
+		
+		System.out.println("Start Learning...");
+		System.out.println("Never Give Up...");
 	}
 
 }
