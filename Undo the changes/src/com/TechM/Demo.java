06d4aa847cc4907b786f7b7c166cb1d032a47394
Rakesh Itape <rakeshitape844@gmail.com>
2022-03-15 16:23:48 +0530

    Revert "1st check 1st"
    This reverts commit 81b8ca8170148e1213a3aafbebab4f5d453cd600.

--- Undo the changes/src/com/TechM/Demo.java
+++ Undo the changes/src/com/TechM/Demo.java
@@ -5,9 +5,7 @@ public class Demo
 	public static void main(String[] args) 
 	{
 		System.out.println("New Demo class");
-		System.out.println("This is Java");
-		System.out.println("This is Python");
-		System.out.println("This is C.");
+		 
 	}
 
 }
